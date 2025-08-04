b14121ec0a726e7b4e96fa568634fb78a13b3e4b
prashant-m4 <prashantsingh090798@gmail.com>
2025-08-05 00:27:16 +0530

    feat: added redlock for distributed systems

--- redlock/src/main/java/com/example/App.java
+++ redlock/src/main/java/com/example/App.java
@@ -2,27 +2,59 @@ package com.example;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.params.SetParams;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
-    private static Counter counter = new Counter();
-    private static String RESOURCE_1_KEY = "resource_1";
+    private static Integer NO_OF_INSTANCES = 20;
+    private static Integer QUORUM = NO_OF_INSTANCES / 2 + 1;
+    private static Counter[] counters = new Counter[NO_OF_INSTANCES];
+    private static String RESOURCE_KEY = "Resource-";
     private static Integer EXPIRATION_DURATION_SECONDS = 10;
 
-    public static void main(String[] args) {
+    static {
+        for (int i = 0; i < NO_OF_INSTANCES; i++) {
+            counters[i] = new Counter("Counter-" + i);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
         Runnable task = () -> {
-            Jedis jedis = new Jedis("localhost", 6379);
             try {
+                Jedis jedis = new Jedis("localhost", 6379, 5000);
                 SetParams setParams = new SetParams();
                 setParams.nx();
                 setParams.ex(EXPIRATION_DURATION_SECONDS);
+                List<Counter> results = new ArrayList<Counter>();
+
                 while (true) {
-                    String result = jedis.set(RESOURCE_1_KEY, Thread.currentThread().getName(), setParams);
-                    if ("OK".equals(result)) {
-                        System.out.println("Locked acquired by " + Thread.currentThread().getName());
-                        performCriticalSection(counter);
-                        jedis.del(RESOURCE_1_KEY);
+                    for (Counter counter : counters) {
+                        String result = jedis.set(RESOURCE_KEY + counter.getName(), Thread.currentThread().getName(),
+                                setParams);
+                        if (!"OK".equals(result)) {
+                            results.add(counter);
+                        } else {
+                            System.out.println(
+                                    "Locked acquired by " + Thread.currentThread().getName() + " on Counter "
+                                            + counter.getName());
+                        }
+                    }
+
+                    if (results.size() >= QUORUM) {
+                        for (Counter counter : results) {
+                            jedis.del(RESOURCE_KEY + counter.getName());
+                            jedis.set(RESOURCE_KEY + counter.getName(), Thread.currentThread().getName(), setParams);
+                            System.out.println(
+                                    "Locked acquired by " + Thread.currentThread().getName() + " on Counter "
+                                            + counter.getName());
+                        }
+                        for (Counter counter : counters) {
+                            performCriticalSection(counter);
+                            jedis.del(RESOURCE_KEY + counter.getName());
+                            System.out.println("Locked released by " + Thread.currentThread().getName()
+                                    + " on Counter " + counter.getName());
+                        }
                         jedis.close();
-                        System.out.println("Locked released by " + Thread.currentThread().getName());
                         break;
                     }
                 }
@@ -31,16 +63,24 @@ public class App {
             }
         };
 
-        Thread thread1 = new Thread(task, "Thread1");
-        Thread thread2 = new Thread(task, "Thread2");
+        Thread thread1 = new Thread(task, "Thread-1");
+        Thread thread2 = new Thread(task, "Thread-2");
 
         thread1.start();
         thread2.start();
+        thread1.join();
+        thread2.join();
+        for (Counter counter : counters) {
+            System.out.println(counter.getCount());
+        }
+
     }
 
     private static void performCriticalSection(Counter counter) throws InterruptedException {
         Thread.sleep(1000);
-        counter.increment();
+        for (int i = 0; i < 10; i++) {
+            counter.increment();
+        }
         Thread.sleep(1000);
     }
 }
--- redlock/src/main/java/com/example/Counter.java
+++ redlock/src/main/java/com/example/Counter.java
@@ -1,7 +1,20 @@
 package com.example;
 
 public class Counter {
+    private String name = "";
     private int counter = 0;
+
+    public Counter(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getCount() {
+        return this.counter;
+    }
 
     public void increment() {
         counter++;
